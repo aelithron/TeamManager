6b644126fcceb02cd60d1dbe32b63808dab7ae24
Aelithron <aelithron@gmail.com>
2025-03-29 06:25:53 -0600

    Work on the menu some more
    Signed-off-by: Aelithron <aelithron@gmail.com>

--- src/main/java/com/aelithron/teamManager/TeamCMD.java
+++ src/main/java/com/aelithron/teamManager/TeamCMD.java
@@ -1,11 +1,14 @@
 package com.aelithron.teamManager;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 public class TeamCMD implements CommandExecutor {
@@ -20,12 +23,48 @@ public class TeamCMD implements CommandExecutor {
             sender.sendMessage(PluginTools.getInstance().getPrefix() + ChatColor.RED + "You must be a player to use this command!");
             return false;
         }
-
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            player.openInventory(createMainMenu(player));
+            return true;
+        }
         return false;
     }
 
-    private Inventory createMenu(Player player) {
-        Inventory inv = plugin.getServer().createInventory(player, 9, "Team Menu");
+    private Inventory createMainMenu(Player player) {
+        Inventory inv = plugin.getServer().createInventory(player, 36, PluginTools.getInstance().getPrefix() + ChatColor.AQUA + "Team Menu");
+
+        ItemStack placeholder = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+        ItemMeta placeholderMeta = placeholder.getItemMeta();
+        placeholderMeta.setDisplayName(ChatColor.GRAY + " ");
+        placeholder.setItemMeta(placeholderMeta);
+        for (int i : new int[]{27, 29, 31, 33, 35}) {
+            inv.setItem(i, placeholder);
+        }
+
+        ItemStack navArrow = new ItemStack(Material.ARROW);
+        ItemMeta navArrowMeta = navArrow.getItemMeta();
+        navArrowMeta.setDisplayName(ChatColor.RED + "Back");
+        navArrow.setItemMeta(navArrowMeta);
+        inv.setItem(28, navArrow);
+
+        ItemStack createTeam = new ItemStack(Material.GREEN_DYE);
+        ItemMeta createTeamMeta = createTeam.getItemMeta();
+        createTeamMeta.setDisplayName(ChatColor.GREEN + "Create/Manage Team");
+        createTeam.setItemMeta(createTeamMeta);
+        inv.setItem(30, createTeam);
+
+        ItemStack close = new ItemStack(Material.BARRIER);
+        ItemMeta closeMeta = close.getItemMeta();
+        closeMeta.setDisplayName(ChatColor.RED + "Close");
+        close.setItemMeta(closeMeta);
+        inv.setItem(32, close);
+
+        ItemStack navArrow2 = new ItemStack(Material.ARROW);
+        ItemMeta navArrowMeta2 = navArrow2.getItemMeta();
+        navArrowMeta2.setDisplayName(ChatColor.GREEN + "Next");
+        navArrow2.setItemMeta(navArrowMeta2);
+        inv.setItem(34, navArrow2);
 
         return inv;
     }
--- src/main/java/com/aelithron/teamManager/TeamManager.java
+++ src/main/java/com/aelithron/teamManager/TeamManager.java
@@ -4,6 +4,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 public final class TeamManager extends JavaPlugin {
@@ -34,16 +35,30 @@ public final class TeamManager extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    private boolean writeTeam(UUID team, Team teamObj) {
-
-        return false;
+    Team readTeam(UUID teamID) {
+        try {
+            return (Team) dataStore.get(teamID.toString());
+        } catch (ClassCastException e) {
+            return null;
+        }
     }
 
-    public void reloadDataStore() {
+    boolean writeTeam(Team team) {
+        try {
+            reloadDataStore();
+            dataStore.set(team.getId().toString(), team);
+            saveDataStore();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    void reloadDataStore() {
         dataStore = YamlConfiguration.loadConfiguration(dataStoreFile);
     }
-
-    public void saveDataStore() {
+    void saveDataStore() {
         try {
             dataStore.save(new File(getDataFolder(), "data.yml"));
         } catch (IOException e) {
